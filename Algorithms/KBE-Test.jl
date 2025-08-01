73ceecbfa97b4c73f1cdd33d239f2e2de7823c20
nsalazard <nsalazard@unal.edu.co>
2025-07-31 23:02:41 -0400

    KBE

---
+++ Algorithms/KBE-Test.jl
@@ -0,0 +1,306 @@
+if length(ARGS) < 2
+    println("Usage: julia run_tdnegf.jl <n> <t_end>")
+    exit(1)
+end
+
+
+### Global parameters 
+const L = parse(Int, ARGS[1])
+tmax= parse(Float64, ARGS[2])# Library
+
+using KadanoffBaym, LinearAlgebra, BlockArrays
+using Bessels
+using PyPlot
+using Tullio
+⊗(A,B) = kron(A,B)
+
+## Parameters and predifined quantities
+hbar = 1.#0.658211928e0  
+### Pauli matrices
+σ_0 = Matrix{ComplexF64}([1. 0. ; 0 1])
+σ_x =  Matrix{ComplexF64}([0 1; 1 0])
+σ_y =  Matrix{ComplexF64}([0 -1im ; 1im 0 ])
+σ_z = Matrix{ComplexF64}([1 0. ; 0. -1]) ; 
+
+# Lattice size
+#L = 2
+nσ = 2
+nx = L
+ny = 1
+γ = 1
+γso = 0
+j_sd = 0
+# Allocate the initial Green functions (time arguments at the end)
+GL = GreenFunction(zeros(ComplexF64, nx*nσ, nx*nσ, 1, 1), SkewHermitian)
+GG = GreenFunction(zeros(ComplexF64, nx*nσ, nx*nσ, 1, 1), SkewHermitian);
+# GL_d = GreenFunction(zeros(ComplexF64, L, L, 1, 1), SkewHermitian)
+# GG_d = GreenFunction(zeros(ComplexF64, L, L, 1, 1), SkewHermitian);
+
+## Auxiliary integrator
+
+function integrate0(t1,t2,A)
+    retval = 0.0
+    dt = 0.01
+    ts = t2:dt:t1
+    A_vec = A.(ts)
+    @tullio retval = A_vec[α]
+    retval = retval*dt
+end
+# Auxiliary integrator for the first type of integral
+function integrate1(hs::Vector, t1, t2, A::GreenFunction, B::GreenFunction, C::GreenFunction; tmax=t1)
+    retval = zero(A[t1, t1])
+    @inbounds for k in 1:tmax
+        @views LinearAlgebra.mul!(retval, A[t1, k] - B[t1, k], C[k, t2], hs[k], 1.0)
+    end
+    return retval
+end
+# Auxiliary integrator for the second type of integral
+function integrate2(hs::Vector, t1, t2, A::GreenFunction, B::GreenFunction, C::GreenFunction; tmax=t2)
+    retval = zero(A[t1, t1])
+    @inbounds for k in 1:tmax
+        @views LinearAlgebra.mul!(retval, A[t1, k], B[k, t2] - C[k, t2], hs[k], 1.0)
+    end
+    return retval
+end
+
+# Self Energy 
+
+#### Dynamical variables 
+Base.@kwdef struct FermiHubbardData2B{T}
+    GL::T
+    GG::T
+    ΣL::T # zero(GL)
+    ΣG::T # zero(GG)
+end
+
+## Calculation of the self-energy 
+function selfenergy(ϵ;γ=1,γc=1.)# thop = thop, t_ls = 1.) 
+    ### Note that this configuration for the self energy can be modified later
+    #thop = global_var.thop
+    Δ = 4 * γ^2 - ϵ^2
+    if real(Δ) > 0
+        Σ = ϵ - im * sqrt(Δ)
+    else
+        if real(ϵ) > 0
+            sgn = 1
+        else
+            sgn = -1
+        end
+        Σ = ϵ - sgn * sqrt(-Δ)
+    end
+    Σ = Σ* (γc^2 / (2 * γ^2))
+    return Σ
+end
+function Gamma_ϵ(ϵ;γ=1,γc=1.)#
+    -2*imag(selfenergy(ϵ;γ=γ,γc=γc))
+end
+#using Bessels
+function Gamma_t(t;γ=1,γc=1.)#
+    γ^2/(γc^2)*γ^2*(besselj(0, 2*γ*t)+ besselj(2, 2*γ*t))#*exp(1im*μ*t)
+    #-2*imag(selfenergy(ϵ;γ=γ,γc=γc))
+end
+function Gamma2_t(t;γ=1,γc=1.)#
+    retval = 0.0
+    dϵ = 0.01
+    ϵs = -2γ:dϵ:2γ
+    Gamma = Gamma_ϵ.(ϵs;γ,γc)
+    e = exp.(-1im*ϵs*t)
+    @tullio retval = Gamma[α]*e[α]
+    retval = retval*dϵ/2pi#/(length(ϵs))
+end
+function fermi_mu(ϵ; μ=0.0, Temp=300)
+    KB = 8.6173324e-5           ### Bolzmann factor
+   1/(1. + exp((ϵ-μ)/(KB*Temp)  ))   
+end
+function SelfL(t;γ=1,γc=1.)
+    retval = 0.0
+    dϵ = 0.01
+    ϵs = -2γ:dϵ:2γ
+    Gamma = Gamma_ϵ.(ϵs;γ,γc)
+    e = exp.(-1im*ϵs*t)
+    f = fermi_mu.(ϵs)
+    @tullio retval = Gamma[α]*e[α]*f[α]
+    retval = 1im*retval*dϵ/2pi
+end
+function SelfG(t;γ=1,γc=1.)
+    retval = 0.0
+    dϵ = 0.01
+    ϵs = -2γ:dϵ:2γ
+    Gamma = Gamma_ϵ.(ϵs;γ,γc)
+    e = exp.(-1im*ϵs*t)
+    f = -fermi_mu.(ϵs) .+ 1
+    @tullio retval = Gamma[α]*e[α]*f[α]
+    retval = -1im*retval*dϵ/2pi
+end
+
+# Callback function for the self-energies
+function self_Lead!(model,data,times,_,_,t,t′)
+    # Unpack data and model 
+    (;GL,GG,ΣL,ΣG) = data
+    (;Δ1, Δ2) = model
+    ∫ds(A) = integrate0(times[t],times[t′], A  ) 
+    # Resize self energies 
+    if (n = size(GL,3)) > size(ΣL,3)
+        resize!(ΣL, n)
+        resize!(ΣG, n)
+    end
+    ### Time dependece of the leads 
+    ϕ1_t = ∫ds(Δ1)#Δ1(times[t], times[t′] )
+    ϕ2_t = ∫ds(Δ2)
+    #Δ2_t = Δ2(times[t], times[t′] )
+    ### Setting the component of the left self-energy
+    self = SelfL(times[t] - times[t′];γ=1.0,γc=1.0)
+    selfL0 = zeros(ComplexF64,L*nσ,L*nσ)
+    selfL0[1:nσ,1:nσ] = self*exp(-1im*ϕ1_t)*diagm(ones(nσ))### Left lead selfenergy 
+    selfL0[end-nσ+1:end,end-nσ+1:end] = self*exp(-1im*ϕ2_t)*diagm(ones(nσ)) ### Right lead selfenergy 
+    ### Setting the component of the right self-energy
+    self = SelfG(times[t] - times[t′];γ=1.0,γc=1.0)
+    selfG0 = zeros(ComplexF64,L*nσ,L*nσ)
+    selfG0[1:nσ,1:nσ] = self*exp(-1im*ϕ1_t)*diagm(ones(nσ)) ### Left lead selfenergy 
+    selfG0[end-nσ+1:end,end-nσ+1:end] = self*exp(-1im*ϕ2_t)*diagm(ones(nσ)) ### Right lead selfenergy 
+
+    ### Define the self energies 
+    ΣL[t, t′] = selfL0#exp()
+    ΣG[t, t′] = selfG0
+end
+
+
+# Hamiltonian 
+
+## Building Hamiltonian
+#### Create electronic Hamiltonian 
+function block_h(;ny=1,γ=1,γso=0.0)
+    #γ::Float64,γso::ComplexF64,Bz::Float64,ny::Int)
+    "Creates the building blocks for a general nx x ny square lattice "
+    dim = ny*2 # We include the spin degree of freedom 
+    ######
+    H0 = zeros(ComplexF64,dim,dim)
+    T  = zeros(ComplexF64,dim,dim)
+    One_y = Diagonal(ones(ny))
+    ######
+    Ty = diagm(-1 =>  ones(ny-1))
+    T0 = Ty⊗(-γ*σ_0 - 1im*γso*σ_x)
+    H0 .= T0 + T0' #-Bz*kron(One_y, σ_z)
+    ######
+    T .= One_y⊗(-γ*σ_0 + γso*1im*σ_y)
+    return H0, T
+end
+
+function hs(vm_i1x::Array{Float64,2};nx=nx,ny=1,γ=1,γso=0.0,j_sd=0.0)
+    "This function build the central hamiltonian wwith two band"
+    #γ::Float64,γso::ComplexF64,Bz::Float64,nx::Int,ny::Int)
+    dim = nx*ny*2 #*2
+    zero = zeros(ComplexF64,nx,nx)
+    HC = zeros(ComplexF64,dim,dim)
+    One_x = Diagonal(ones(nx))
+    H0,T = block_h(;ny,γ,γso)
+    Tx = diagm( -1 =>  ones(nx-1))⊗T 
+    HC = (One_x⊗H0) +  Tx + Tx'
+    ### Local moments
+    for i in range(1,nx) 
+        zero[i,i] = 1.0
+        HC += -j_sd*zero⊗(vm_i1x[i,1]*σ_x
+                    +vm_i1x[i,2]*σ_y
+                    +vm_i1x[i,3]*σ_z)
+        zero[i,i] = 0.0
+    end
+    return HC
+end
+
+## Auxiliary structure with the parameters of the model 
+Base.@kwdef struct FermiOpenModel{T1,T2}
+    # interaction strength
+    # Time dependence of the self energies 
+    Δ1::T1
+    Δ2::T2
+    #U::T
+    nx = nx
+    ny = ny
+    nσ = nσ
+    γ = γ
+    γso =  γso
+    j_sd = j_sd
+    # Initial configuration of the classical vectors 
+    #vm_i1x = vm_i1x#zeros(Float64,  nx, 3)
+    vm_i1x = zeros(Float64,  L, 3);
+    # Hamiltonian of the system 
+    hs = hs(vm_i1x;nx,ny,γ,γso,j_sd)
+    #H_u = h
+    #H_d = h
+end
+
+# Initial conditions 
+
+# Initial conditions
+# Relatively small interaction parameter
+U₀ = 0.05   # Bias
+U₁ = 0
+model = FermiOpenModel(Δ1 = t1-> U₀,Δ2 = t2-> U₁)
+N = zeros(L*nσ)
+#vm_i1x = zeros(Float64,  L, 3)
+# N_d = zeros(L)
+# #######
+N[1:4] = 0.0 .* [1, 1, 1, 1]
+# N_d[1:4] = 0.1 .* [1, 1, 1, 1]
+# #######
+# N_u[5:8] = 0.0 .* [1, 1, 1, 1]
+# N_d[5:8] = 0.0 .* [1, 1, 1, 1]
+# #######
+GL[1, 1] = 1.0im * diagm(N)
+GG[1, 1] = -1.0im * (I - diagm(N)) ;
+ΣL = zero(GL)
+ΣG = zero(GG)
+ΣL[1,1] = SelfL(0;γ=1.0,γc=1.0)*diagm(ones(L*nσ))
+ΣG[1,1] = SelfG(0;γ=1.0,γc=1.0)*diagm(ones(L*nσ))
+data = FermiHubbardData2B(GL=GL, GG=GG, ΣL=ΣL, ΣG=ΣG) ;
+
+# EOMS
+
+# Right-hand side for the "vertical" evolution
+function fv!(model, data, out, times, h1, h2, t, t′)
+    # Unpack data and model
+    (; GL, GG, ΣL, ΣG) = data
+    (; hs, Δ1, Δ2) = model
+
+    # Real-time collision integrals
+    ∫dt1(A, B, C) = integrate1(h1, t, t′, A, B, C)
+    ∫dt2(A, B, C) = integrate2(h2, t, t′, A, B, C)
+    # Equations of motion
+    out[1] = -1.0im * ( hs* GL[t, t′] +
+        ∫dt1(ΣG, ΣL, GL) + ∫dt2(ΣL, GL, GG)) #### For G<_up
+    out[2] = -1.0im * (hs* GG[t, t′] +
+        ∫dt1(ΣG, ΣL, GG) + ∫dt2(ΣG, GL, GG)) #### For G>_up
+    return out
+end
+# Right-hand side for the "diagonal" evolution
+function fd!(model, data, out, times, h1, h2, t, t′)
+    fv!(model, data, out, times, h1, h2, t, t)
+    out[1] .-= adjoint.(out[1])
+    out[2] .-= adjoint.(out[2])
+end
+# Right-hand side for the "diagonal" evolution
+function fd!(model, data, out, times, h1, h2, t, t′)
+    fv!(model, data, out, times, h1, h2, t, t)
+    out .-= adjoint.(out)
+end
+
+# Evolution
+
+# tolerances
+atol = 1e-8#1e-7#
+rtol = 1e-6#1e-4#
+
+# Call the solver
+elapsed_time = @elapsed begin
+sol = kbsolve!(
+    (x...) -> fv!(model, data, x...),
+    (x...) -> fd!(model, data, x...),
+    [data.GL, data.GG],
+    (0.0, tmax);
+    callback = (x...) -> self_Lead!(model, data, x...),
+    atol = atol,
+    rtol = rtol,
+    stop = x -> false
+)
+end
+println("Total time of simulation: ", elapsed_time, " s" )
